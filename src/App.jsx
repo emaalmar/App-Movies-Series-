7201246f2f970a4954b98be42570b74a2b85237a
Emanuel Ledesma <emanuellescam@gmail.com>
2025-08-22 23:08:32 -0600

    Creacion de navbar, sign in, carrusele y conexion a api externa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { SignIn } from './SignIn';
+import { Home } from './Home';
+
+function App() {
+
+  return (
+    <div className="">
+      <Home />
+    </div>
+  )
+}
+
+export default App
