5145bd12255c3454181c68cb10655f31fcd3eb52
Emanuel Ledesma <emanuellescam@gmail.com>
2025-08-23 21:58:44 -0600

    Eliminacion y modificaciones

--- src/components/ProtecttedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-function ProtectedRoute({ isAuthenticated, children }) {
-    return isAuthenticated ? children : <Navigate to="/home" />;
-}
-
-export default ProtectedRoute;
