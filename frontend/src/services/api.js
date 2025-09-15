ab545bc06713f12a39dce992645a291603154ad5
Emanuel Ledesma <emanuellescam@gmail.com>
2025-09-15 13:26:09 -0600

    fix: update baseURL in api.js to use production URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const baseURL = import.meta.env.VITE_API_URL || 'https://app-movies-series-git-main-emanuels-projects-c459fdaa.vercel.app/'
 
 export const api = axios.create({
   baseURL: `${baseURL}/api`,
